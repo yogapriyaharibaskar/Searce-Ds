593b6aa6d23147a50102ae86d0b1beaab9b6ed71
yogapriyaharibaskar <38234467+yogapriyaharibaskar@users.noreply.github.com>
2022-01-25 14:59:38 +0530

    Update max_stack.py

--- day 2/Assignment/max_stack.py
+++ day 2/Assignment/max_stack.py
@@ -1,1 +1,20 @@
 
+stack = []
+max_stack = []
+
+for _ in range(int(input())):
+    
+    cmd = input()
+    
+    if cmd[0] == '1':
+        n = int(cmd.split()[1])
+        stack.append(n)
+        if len(max_stack) == 0 or max_stack[-1] < n:
+            max_stack.append(n)
+        else: 
+            max_stack.append(max_stack[-1])
+    elif cmd == '2':
+        stack.pop()
+        max_stack.pop()
+    else: 
+        print(max_stack[-1])
