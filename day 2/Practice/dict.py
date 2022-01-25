b5db62989422a74d5fb7b52133d662375002894b
yogapriyaharibaskar <38234467+yogapriyaharibaskar@users.noreply.github.com>
2022-01-25 16:00:57 +0530

    Add files via upload

---
+++ day 2/Practice/dict.py
@@ -0,0 +1,72 @@
+my_dict = {} #empty dictionary
+print(my_dict)
+my_dict = {1: 'Python', 2: 'Java'} #dictionary with elements
+print(my_dict)
+
+my_dict = {'First': 'Python', 'Second': 'Java'}
+print(my_dict)
+
+my_dict['Second'] = 'C++' #changing element
+print(my_dict)
+
+my_dict['Third'] = 'Ruby' #adding key-value pair
+print(my_dict)
+
+my_dict = {'First': 'Python', 'Second': 'Java', 'Third': 'Ruby'}
+a = my_dict.pop('Third') #pop element
+print('Value:', a)
+print('Dictionary:', my_dict)
+
+b = my_dict.popitem() #pop the key-value pair
+print('Key, value pair:', b)
+print('Dictionary', my_dict)
+
+my_dict.clear() #empty dictionary
+print('n', my_dict)
+
+
+## nested dictionary
+#Create a Nested Dictionary
+people = {1: {'name': 'John', 'age': '27', 'sex': 'Male'},
+          2: {'name': 'Marie', 'age': '22', 'sex': 'Female'}}
+#Access elements of a Nested Dictionary
+print(people)
+
+people = {1: {'name': 'John', 'age': '27', 'sex': 'Male'},
+          2: {'name': 'Marie', 'age': '22', 'sex': 'Female'}}
+
+print(people[1]['name'])
+print(people[1]['age'])
+print(people[1]['sex'])
+
+#Add element to a Nested Dictionary
+people = {1: {'name': 'John', 'age': '27', 'sex': 'Male'},
+          2: {'name': 'Marie', 'age': '22', 'sex': 'Female'}}
+
+people[3] = {}
+
+people[3]['name'] = 'Luna'
+people[3]['age'] = '24'
+people[3]['sex'] = 'Female'
+people[3]['married'] = 'No'
+
+print(people[3])
+
+#delete dictionary from a nested dictionary
+people = {1: {'name': 'John', 'age': '27', 'sex': 'Male'},
+          2: {'name': 'Marie', 'age': '22', 'sex': 'Female'},
+          3: {'name': 'Luna', 'age': '24', 'sex': 'Female'},
+          4: {'name': 'Peter', 'age': '29', 'sex': 'Male'}}
+
+del people[3], people[4]
+print(people)
+
+#iterate through a Nested dictionary
+people = {1: {'Name': 'John', 'Age': '27', 'Sex': 'Male'},
+          2: {'Name': 'Marie', 'Age': '22', 'Sex': 'Female'}}
+
+for p_id, p_info in people.items():
+    print("\nPerson ID:", p_id)
+    
+    for key in p_info:
+        print(key + ':', p_info[key])
